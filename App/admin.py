1c093031ce2e5430a34e52a25a3ac3fe07be2b22
MansAlien <mans012033945421@gmail.com>
2024-01-12 19:59:29 +0200

    adding the table and connect it with the database

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'occupation', 'salary', 'gender']
+    search_fields = ['name']
+    list_per_page = 8
+
+admin.site.register(Employee, EmployeeAdmin)
--- App/models.py
+++ App/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    GENDER = (
+        ('M','M'),
+        ('F','F')
+    )
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    occupation = models.CharField(max_length=100)
+    salary = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, null=True, choices=GENDER)
+
+    def __str__(self):
+        return self.name
